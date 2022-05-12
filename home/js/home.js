c56c8981e444adbef658eba8b838a75ffef90a03
truongjae <truongjae@gmail.com>
2022-05-12 13:43:59 +0700

    update

--- home/js/home.js
+++ home/js/home.js
@@ -1,4 +1,5 @@
 const server= "https://66bf-8-219-3-31.ngrok.io";
+// const server = "http://localhost";
 function addHours(numOfHours, date = new Date()) {
     date.setTime(date.getTime() + numOfHours * 60 * 60 * 1000);
 
